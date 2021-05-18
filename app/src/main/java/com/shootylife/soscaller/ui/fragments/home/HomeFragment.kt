12140376d34cbf35fc86a2a1f30a725e15a9b8de
Louis <louis.dutoitld@gmail.com>
2021-05-18 18:39:07 +0200

    calls

--- app/src/main/java/com/shootylife/soscaller/ui/fragments/home/HomeFragment.kt
+++ app/src/main/java/com/shootylife/soscaller/ui/fragments/home/HomeFragment.kt
@@ -1,14 +1,17 @@
 package com.shootylife.soscaller.ui.fragments.home
 
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
+import android.widget.Button
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import com.shootylife.soscaller.R
+
 
 class HomeFragment : Fragment() {
 
@@ -25,5 +28,25 @@ class HomeFragment : Fragment() {
         homeViewModel.text.observe(viewLifecycleOwner, Observer {
         })
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        var pompiers = view.findViewById<Button>(R.id.btn_pompiers).setOnClickListener{
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "18"))
+            startActivity(intent)
+        }
+        var urgences = view.findViewById<Button>(R.id.btn_urgences).setOnClickListener{
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "112"))
+            startActivity(intent)
+        }
+        var police = view.findViewById<Button>(R.id.btn_police).setOnClickListener{
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "17"))
+            startActivity(intent)
+        }
+        var samu = view.findViewById<Button>(R.id.btn_samu).setOnClickListener{
+            val intent = Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + "15"))
+            startActivity(intent)
+        }
     }
 }
