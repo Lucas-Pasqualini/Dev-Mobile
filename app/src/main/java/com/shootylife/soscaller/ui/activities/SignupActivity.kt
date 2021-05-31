467e88a02db60186c648598546925856946fd911
denisdalmeida04@gmail.com <denisdalmeida04@gmail.com>
2021-05-31 17:12:39 +0200

    feat: signup

---
+++ app/src/main/java/com/shootylife/soscaller/ui/activities/SignupActivity.kt
@@ -0,0 +1,23 @@
+package com.shootylife.soscaller.ui.activities
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shootylife.soscaller.R
+import com.shootylife.soscaller.databinding.ActivityLoginBinding
+import com.shootylife.soscaller.utils.activityViewBinding
+
+class SignupActivity : AppCompatActivity() {
+    private var _binding by activityViewBinding(ActivityLoginBinding::inflate)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+
+        _binding.button.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+    }
+}
