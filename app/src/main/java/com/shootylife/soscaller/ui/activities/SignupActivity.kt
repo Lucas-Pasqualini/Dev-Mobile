f88b6ce4b2c17e4b37c8c946dd6fea9fffa8d7b5
denisdalmeida04@gmail.com <denisdalmeida04@gmail.com>
2021-06-05 17:24:15 +0200

    signup

---
+++ app/src/main/java/com/shootylife/soscaller/ui/activities/SignupActivity.kt
@@ -0,0 +1,12 @@
+package com.shootylife.soscaller.ui.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.shootylife.soscaller.R
+
+class SignupActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_signup)
+    }
+}
